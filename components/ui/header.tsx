b2a6afab3fb009d385ed564482d133cbb1f27818
chrisp <cmpurman@gmail.com>
2024-04-16 14:35:03 -0400

    updates

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
-            {/* Desktop sign in links */}
+
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
                 <Link
